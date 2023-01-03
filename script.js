ac09f2e3e32f45925e9bc831da0196bf812a0c1c
Lucas Lima <lucaswilliamml@gmail.com>
2023-01-03 08:09:51 -0300

    Upload project

---
+++ script.js
@@ -0,0 +1,33 @@
+function clean() {
+    document.getElementById('result').innerHTML = "";
+}
+
+function back() {
+    var number = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = number.substring(0, number.length - 1)
+
+}
+
+function insert(input_number) {
+    var number = document.getElementById('result').innerHTML;
+    var lastdigit = document.getElementById('result').innerHTML = number.slice(-1);
+    if ((lastdigit == '+' || lastdigit== '-' || lastdigit == '/' || lastdigit == '*') && (input_number == '+' || input_number == '-' || input_number == '/' || input_number == '*')){
+        document.getElementById('result').innerHTML = number.substring(0, number.length - 1) + input_number;
+    }
+    else{
+    document.getElementById('result').innerHTML = number + input_number;
+    }
+
+
+}
+
+function calculate() {
+    var number = document.getElementById('result').innerHTML;
+    if (number) {
+        document.getElementById('result').innerHTML = eval(number)
+    }
+    else{
+        document.getElementById('result').innerHTML = ""
+    }
+
+}
